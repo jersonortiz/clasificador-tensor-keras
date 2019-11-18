8fa5b519ad519848d70306de9fdd9ebb87a35d86
jerson ortiz <jersonandresoc@ufps.edu.co>
2019-11-18 15:25:44 -0500

    ;

--- controller.php
+++ controller.php
@@ -2,7 +2,7 @@
 	$archivo = $_GET['cumplim'];
 	$var = "file".$archivo.".jpg";
 	echo $var;
-	$result = exec("python3 predict.py".$var);
+	$result = exec("python3 predict.py".$var)
 	echo $result;
 
 ?>
