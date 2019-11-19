43cb7b03a7a9520a94ac059b519d69871f6b6b0f
jerson ortiz <jersonandresoc@ufps.edu.co>
2019-11-19 14:33:47 -0500

    algo

--- controller.php
+++ controller.php
@@ -3,12 +3,14 @@
 	ini_set('display_errors', '1');
 	$archivo = $_GET['cumplim'];
 	$var = "file".$archivo.".jpg";
-	echo $var;
+
 	$resu="";
 	$command ="python3 predict.py ".$var." 2>&1";
-echo $command."<br>";
+
 	$result = exec($command,$resu);
-	echo $result;
-	print_r($resu);
+	$json = json_encode($resu;
+    print_r($json);
+
+
 
 ?>
